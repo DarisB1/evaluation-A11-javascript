3357ba37d813a3fd890947fed27fda9d8c883dde
soldier <dboumertit@edenschool.fr>
2024-11-15 14:30:35 +0100

    feat(app): verif

--- ex09.js
+++ ex09.js
@@ -1,16 +1,16 @@
-function encodePassword(password, encodeFunction) {
-	return encodeFunction(password)
-}
+// function encodePassword(password, encodeFunction) {
+// 	return encodeFunction(password)
+// }
 
-function encodeRot8(password) {
+// function encodeRot8(password) {
 	
-}
+// }
 
-function encodeRot16(password) {
+// function encodeRot16(password) {
 	
-}
+// }
 
-console.log(encodePassword("abc", encodeRot8));
-console.log(encodePassword("abc", encodeRot16));
-console.log(encodePassword("xyz", encodeRot8));
-console.log(encodePassword("xyz", encodeRot16));
+// console.log(encodePassword("abc", encodeRot8));
+// console.log(encodePassword("abc", encodeRot16));
+// console.log(encodePassword("xyz", encodeRot8));
+// console.log(encodePassword("xyz", encodeRot16));
